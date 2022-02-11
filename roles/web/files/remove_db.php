5e34693c69b6eec6a23871a569096bba925fe084
jarjar <jarjar@pop-os.localdomain>
2022-02-11 15:51:03 +0100

    update

--- roles/web/files/remove_db.php
+++ roles/web/files/remove_db.php
@@ -1,14 +1,5 @@
 <?php
-   $dbhost = 'mysql';
-   $dbuser = 'root';
-   $dbpass = 'root';
-   $conn = mysql_connect($dbhost, $dbuser, $dbpass);
-   
-   if(! $conn ) {
-      die('Could not connect: ' . mysql_error());
-   }
-   
-   echo "Connected successfully\n";
+   require 'bddconf.php';
    
    $sql = 'DROP Database test_db';
    $retval = mysql_query( $sql, $conn );
